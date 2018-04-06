5a7914bf10bdf70e851549f54b5ce91f57a75cc8
fusionjack <dogfight60-fusionjack@yahoo.de>
2018-04-07 01:13:02 +0200

    Change default host file to gitlab one
    https://gitlab.com/fusionjack/host/raw/master/hosts.txt

--- app/src/main/java/com/fusionjack/adhell3/utils/AdhellAppIntegrity.java
+++ app/src/main/java/com/fusionjack/adhell3/utils/AdhellAppIntegrity.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Set;
 
 public class AdhellAppIntegrity {
-    public static final String ADHELL_STANDARD_PACKAGE = "https://goo.gl/soJyHa";
+    public static final String ADHELL_STANDARD_PACKAGE = "https://bit.ly/2HfsqkV";
     public static final int BLOCK_URL_LIMIT = 15000;
     public final static String DEFAULT_POLICY_ID = "default-policy";
 
