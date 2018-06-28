d24ab8ec7e5c21de8381e5868068be3a8582a53c
fusionjack <dogfight60-fusionjack@yahoo.de>
2018-06-28 21:01:26 +0200

    Don't prefix blacklist and whitelist with wildcard
    Let the user controls how the domain should be defined.

--- app/src/main/java/com/fusionjack/adhell3/db/entity/UserBlockUrl.java
+++ app/src/main/java/com/fusionjack/adhell3/db/entity/UserBlockUrl.java
@@ -6,7 +6,6 @@ import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.TypeConverters;
 
 import com.fusionjack.adhell3.db.DateConverter;
-import com.fusionjack.adhell3.utils.BlockUrlPatternsMatch;
 
 import java.util.Date;
 
@@ -25,7 +24,7 @@ public class UserBlockUrl {
     public Date insertedAt;
 
     public UserBlockUrl(String url, Date insertedAt) {
-        this.url = BlockUrlPatternsMatch.getValidKnoxUrl(url);
+        this.url = url;
         this.insertedAt = insertedAt;
     }
 }
