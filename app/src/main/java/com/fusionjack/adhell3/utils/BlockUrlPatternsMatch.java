070646f813150ebbed71b31f2e2aca91a3d15dc1
MMotti <matthew.w.motti@gmail.com>
2018-07-25 14:17:52 +0100

    Remove unnecessary imports

--- app/src/main/java/com/fusionjack/adhell3/utils/BlockUrlPatternsMatch.java
+++ app/src/main/java/com/fusionjack/adhell3/utils/BlockUrlPatternsMatch.java
@@ -1,9 +1,6 @@
 package com.fusionjack.adhell3.utils;
 
 import com.fusionjack.adhell3.BuildConfig;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
