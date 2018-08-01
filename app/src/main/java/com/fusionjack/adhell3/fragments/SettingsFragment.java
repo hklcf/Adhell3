ad0128913249d1ac1f58d8bbcad2a810313b3f9b
fusionjack <dogfight60-fusionjack@yahoo.de>
2018-08-02 00:31:27 +0200

    Rework Settings UI
    Use PreferenceScreen to add settings in future easier.

--- app/src/main/java/com/fusionjack/adhell3/fragments/SettingsFragment.java
+++ app/src/main/java/com/fusionjack/adhell3/fragments/SettingsFragment.java
@@ -9,12 +9,12 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.fusionjack.adhell3.BuildConfig;
@@ -27,77 +27,74 @@ import com.fusionjack.adhell3.utils.AdhellFactory;
 
 import java.lang.ref.WeakReference;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
     private Context context;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.settings_preference, rootKey);
         this.context = getContext();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_app_settings, container, false);
+    public boolean onPreferenceTreeClick(Preference preference) {
+        switch (preference.getKey()) {
+            case "delete_preference": {
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_question, (ViewGroup) getView(), false);
+                TextView titlTextView = dialogView.findViewById(R.id.titleTextView);
+                titlTextView.setText(R.string.delete_app_dialog_title);
+                TextView questionTextView = dialogView.findViewById(R.id.questionTextView);
+                questionTextView.setText(R.string.delete_app_dialog_text);
 
-        Button deleteAppButton = view.findViewById(R.id.deleteApp);
-        deleteAppButton.setOnClickListener(v -> {
-            View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_question, (ViewGroup) getView(), false);
-            TextView titlTextView = dialogView.findViewById(R.id.titleTextView);
-            titlTextView.setText(R.string.delete_app_dialog_title);
-            TextView questionTextView = dialogView.findViewById(R.id.questionTextView);
-            questionTextView.setText(R.string.delete_app_dialog_text);
+                new AlertDialog.Builder(context)
+                        .setView(dialogView)
+                        .setPositiveButton(android.R.string.yes, (dialog, whichButton) -> {
+                            ContentBlocker contentBlocker = ContentBlocker56.getInstance();
+                            contentBlocker.disableDomainRules();
+                            contentBlocker.disableFirewallRules();
+                            ComponentName devAdminReceiver = new ComponentName(context, CustomDeviceAdminReceiver.class);
+                            DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
+                            dpm.removeActiveAdmin(devAdminReceiver);
+                            Intent intent = new Intent(Intent.ACTION_DELETE);
+                            String packageName = "package:" + BuildConfig.APPLICATION_ID;
+                            intent.setData(Uri.parse(packageName));
+                            startActivity(intent);
+                        })
+                        .setNegativeButton(android.R.string.no, null).show();
+                break;
+            }
+            case "backup_preference": {
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_question, (ViewGroup) getView(), false);
+                TextView titlTextView = dialogView.findViewById(R.id.titleTextView);
+                titlTextView.setText(R.string.backup_database_dialog_title);
+                TextView questionTextView = dialogView.findViewById(R.id.questionTextView);
+                questionTextView.setText(R.string.backup_database_dialog_text);
 
-            new AlertDialog.Builder(context)
-                    .setView(dialogView)
-                    .setPositiveButton(android.R.string.yes, (dialog, whichButton) -> {
-                        ContentBlocker contentBlocker = ContentBlocker56.getInstance();
-                        contentBlocker.disableDomainRules();
-                        contentBlocker.disableFirewallRules();
-                        ComponentName devAdminReceiver = new ComponentName(context, CustomDeviceAdminReceiver.class);
-                        DevicePolicyManager dpm = (DevicePolicyManager) context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-                        dpm.removeActiveAdmin(devAdminReceiver);
-                        Intent intent = new Intent(Intent.ACTION_DELETE);
-                        String packageName = "package:" + BuildConfig.APPLICATION_ID;
-                        intent.setData(Uri.parse(packageName));
-                        startActivity(intent);
-                    })
-                    .setNegativeButton(android.R.string.no, null).show();
-        });
+                new AlertDialog.Builder(context)
+                        .setView(dialogView)
+                        .setPositiveButton(android.R.string.yes, (dialog, whichButton) ->
+                                new BackupDatabaseAsyncTask(getActivity()).execute()
+                        )
+                        .setNegativeButton(android.R.string.no, null).show();
+                break;
+            }
+            case "restore_preference": {
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_question, (ViewGroup) getView(), false);
+                TextView titlTextView = dialogView.findViewById(R.id.titleTextView);
+                titlTextView.setText(R.string.restore_database_dialog_title);
+                TextView questionTextView = dialogView.findViewById(R.id.questionTextView);
+                questionTextView.setText(R.string.restore_database_dialog_text);
 
-        Button backupDatabaseButton = view.findViewById(R.id.backup_database);
-        backupDatabaseButton.setOnClickListener(view1 -> {
-            View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_question, (ViewGroup) getView(), false);
-            TextView titlTextView = dialogView.findViewById(R.id.titleTextView);
-            titlTextView.setText(R.string.backup_database_dialog_title);
-            TextView questionTextView = dialogView.findViewById(R.id.questionTextView);
-            questionTextView.setText(R.string.backup_database_dialog_text);
-
-            new AlertDialog.Builder(context)
-                    .setView(dialogView)
-                    .setPositiveButton(android.R.string.yes, (dialog, whichButton) ->
-                            new BackupDatabaseAsyncTask(getActivity()).execute()
-                    )
-                    .setNegativeButton(android.R.string.no, null).show();
-        });
-
-        Button restoreDatabaseButton = view.findViewById(R.id.restore_database);
-        restoreDatabaseButton.setOnClickListener(view2 -> {
-            View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_question, (ViewGroup) getView(), false);
-            TextView titlTextView = dialogView.findViewById(R.id.titleTextView);
-            titlTextView.setText(R.string.restore_database_dialog_title);
-            TextView questionTextView = dialogView.findViewById(R.id.questionTextView);
-            questionTextView.setText(R.string.restore_database_dialog_text);
-
-            new AlertDialog.Builder(context)
-                    .setView(dialogView)
-                    .setPositiveButton(android.R.string.yes, (dialog, whichButton) ->
-                            new RestoreDatabaseAsyncTask(getActivity()).execute()
-                    )
-                    .setNegativeButton(android.R.string.no, null).show();
-        });
-
-        return view;
+                new AlertDialog.Builder(context)
+                        .setView(dialogView)
+                        .setPositiveButton(android.R.string.yes, (dialog, whichButton) ->
+                                new RestoreDatabaseAsyncTask(getActivity()).execute()
+                        )
+                        .setNegativeButton(android.R.string.no, null).show();
+                break;
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     private static class BackupDatabaseAsyncTask extends AsyncTask<Void, Void, String> {
