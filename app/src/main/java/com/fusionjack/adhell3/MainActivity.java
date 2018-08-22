ca6701b5a99ed4c603d8bdfa4f33f7db9110b14e
fusionjack <dogfight60-fusionjack@yahoo.de>
2018-08-22 23:31:33 +0200

    Use native bottom navigation view

--- app/src/main/java/com/fusionjack/adhell3/MainActivity.java
+++ app/src/main/java/com/fusionjack/adhell3/MainActivity.java
@@ -1,10 +1,14 @@
 package com.fusionjack.adhell3;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AlertDialog;
@@ -28,7 +32,8 @@ import com.fusionjack.adhell3.utils.CrashHandler;
 import com.fusionjack.adhell3.utils.DeviceAdminInteractor;
 import com.fusionjack.adhell3.utils.LogUtils;
 import com.fusionjack.adhell3.utils.PasswordStorage;
-import com.roughike.bottombar.BottomBar;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getCanonicalName();
@@ -79,9 +84,14 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        BottomBar bottomBar = findViewById(R.id.bottomBar);
-        bottomBar.setTabTitleTextAppearance(R.style.bottomBarTextView);
-        bottomBar.setOnTabSelectListener(this::onTabSelected, false);
+        BottomNavigationView bottomBar = findViewById(R.id.bottomBar);
+        bottomBar.setOnNavigationItemSelectedListener(item -> {
+            onTabSelected(item.getItemId());
+            return true;
+        });
+
+        // Nasty workaround to show text and icon if the tab count more than 3
+        removeShiftMode(bottomBar);
     }
 
     @Override
@@ -218,5 +228,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
         return passwordDialog;
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void removeShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                //noinspection RestrictedApi
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                //noinspection RestrictedApi
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("BottomNav", "Unable to get shift mode field", e);
+        } catch (IllegalAccessException e) {
+            Log.e("BottomNav", "Unable to change value of shift mode", e);
+        }
     }
 }
